1f5474d6b6ceac2ff279f7d697a3a987b7afd45d
QuietkidAK <aniketkundu12072004@gmail.com>
2023-12-12 10:06:49 +0530

    edit files

--- matlabfiles/init.py
+++ matlabfiles/init.py
@@ -88,24 +88,40 @@ hold off
 title('Constrained Maxima/Minima')"""
 
 volumeofsolidofrevolution = """clc
+clc
 clear all
 syms x
-f = input('Enter the function: ');
-fL = input('Enter the interval on which the function is defined: '); 
-yr = input('Enter the axis of rotation y = c (enter only c value): '); 
-iL = input('Enter the integration limits: ');
-volume = pi * int((f-yr)^2, iL(1), il(2));
-disp(['Volume is :',num2str(double(volume))])
-f(x) = inline(vectorize(f));
-xvals = linspace(fL(1), fL(2), 201);
-xvalr = flipr(rivals);
-xlivs = fx(xlim);
-figure("Position", [100 200 560 420])
+
+f=input('Enter the function: ');
+fL=input('Enter the interval on which the function is defined');
+yr=input('Enter the axis of rotation y=c(Enter only c value): ');
+iL=input('Enter the integration limits: ');
+Volume=pi*int((f-yr)^2, iL(1), iL(2));
+disp(['Volume is: ', num2str(double(Volume))])
+
+fx=inline(vectorize(f));
+xvals=linspace(fL(1),fL(2),201);
+xvalsr=fliplr(xvals);
+xivals=linspace(iL(1),iL(2),201);
+xivalsr=fliplr(xivals);
+xlim=[fL(1) fL(2)+0.5];
+ylim=fx(xlim);
+
+figure('Position',[100 200 560 420])
 subplot(2,1,1)
 hold on;
-plot(rvals, fx(xvals), '-b', 'linewidth',2);
-[x, y, z ]= cylinder(fx(xvals) - yr, 100);
-figure('Position', [700 200 560 420])
+plot(xvals,fx(xvals), '-b','Linedth',2)
+
+[X,Y,Z]=cylinder(fx(xivals)-yr,100);
+figure('Position',[799 299 550 420])
+Z=iL(1) +Z.*(iL(2)-iL(1));
+surf(Z,Y+yr,X,'EdgeColor','none','FaceColor','flat','FaceAlpha', 0.6)
+hold on;
+plot([iL(1), iL(2)], [yr yr], '-r', 'LineWidth',2);
+xlabel('X-axis');
+ylabel('Y-axis');
+zlabel('Z-axis');
+view(22,11);
 
 """
 area = """clc
@@ -145,6 +161,55 @@ hold on
 e = ezplot(fxx,D);
 legend('f', 'fx', 'fxx')
 legend('location', 'north east outside')"""
+
+lagrange = 
+"""
+%Modified program for Lagranges Multiplier Method
+clear
+clc
+syms x y z L
+f=input('Enter the function f(x,y):');
+g=input('Enter the constrained funtion g(x,y):');
+F=f+L*g;
+gradF=jacobian(F,[x y]);
+S=solve(g,gradF(1),gradF(2),'Real',true); %Solving only for real L,x,y 
+St_pts=double([S.x,S.y]); %Matrix with stationary points in rows.
+h1=St_pts(:,1);
+h2=St_pts(:,2);
+X=[h1];
+Y=[h2];
+disp('Stationary points are:')
+STP=table(X,Y);
+range=double([min(S.x)-3 max(S.x)+3 min(S.y)-3 max(S.y)+3]);%setting plot range 
+[n,m]=size(St_pts);
+for i = 1:n
+F(i)=subs(f,{x,y},{S.x(i),S.y(i)});
+%Finding values of f at all the stationary points
+end
+if n>1
+F_max=max(F);
+disp(['The maximum value of f under the given constraint g is:' char(F_max)]);
+F_min=min(F);
+disp(['The minimum value of f under the given constraint g is:' char(F_min)]);
+else
+disp(['The extremum value of f under the given constraint g is:' char(F)]);
+end
+fmesh(f,range);
+hold on
+h=ezplot(g,range);
+tmp = get(h,'contourMatrix');
+xdt = tmp(1,2:end);% Avoiding first x-data point
+ydt = tmp(2,2:end);% Avoiding first y-data point
+zdt = double(subs(f,{x,y},{xdt,ydt}));
+plot3(xdt,ydt,zdt,'-r','LineWidth',2);
+axis(range);
+for i = 1:numel(x)
+plot3(S.x(i),S.y(i),F(i),'*k','MarkerSize',20);
+end
+hold off
+title('Constrained Maxima/Minima')
+"""
+
 i = input()
 try:
     pp.copy(eval(i))
